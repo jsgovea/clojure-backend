e9445ee51c12df0f0aac1d802c40fea75b62b08c
Jorge Soto <jorgealejandro2116@gmail.com>
2023-07-12 09:57:14 -0700

    💻 Added more example functions

--- src/webdev/core.clj
+++ src/webdev/core.clj
@@ -2,7 +2,8 @@
   (:require [ring.adapter.jetty :as jetty]
             [ring.middleware.reload :refer [wrap-reload]]
             [compojure.core :refer [defroutes GET]]
-            [compojure.route :refer [not-found]]))
+            [compojure.route :refer [not-found]]
+            [ring.handler.dump :refer [handle-dump]]))
 
 (defn greet [req]
   {:status 200
@@ -14,10 +15,46 @@
    :body "Goodbye cruel world!"
    :headers {}})
 
+(defn about [req]
+  {:status 200
+   :body "Hello, I am Jorge and I'm learning clojure"
+   :headers {}})
+
+(defn yo [req]
+  (let [name (get-in req [:route-params :name])]
+    {:status 200
+     :body (str "Yo! " name "!")
+     :headers {}}))
+
+(def ops
+  {"+" +
+   "-" -
+   "*" *
+   ":" /})
+
+
+(defn calc [req]
+  (let [first-value (Integer. (get-in req [:route-params :first-value]))
+        second-value (Integer. (get-in req [:route-params :second-value]))
+        symbol (get-in req [:route-params :symbol])
+        operation (get ops symbol)]
+    (if operation
+      {:status 200
+       :body (str (operation first-value second-value))
+       :headers {}}
+      {:status 400
+       :body (str "Unkown operator: " symbol)
+       :headers {}})))
+
+
 
 (defroutes app
   (GET "/" [] greet)
   (GET "/goodbye" [] goodbye)
+  (GET "/about" [] about)
+  (GET "/yo/:name" [] yo)
+  (GET "/calc/:first-value/:symbol/:second-value" [] calc)
+  (GET "/request" [] handle-dump)
   (not-found "Page not found."))
 
 (defn -main [port]
