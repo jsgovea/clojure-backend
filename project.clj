453b456e47713574836f574bfdc88cb01fd5fb70
Jorge Soto <jorgealejandro2116@gmail.com>
2023-07-12 13:49:15 -0700

    👨‍💻 Added db table

--- project.clj
+++ project.clj
@@ -5,10 +5,12 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [ring "1.10.0"]
-                 [compojure "1.7.0"]]
+                 [compojure "1.7.0"]
+                 [org.clojure/java.jdbc "0.7.12"]
+                 [org.postgresql/postgresql "42.6.0"]]
   :repl-options {:init-ns webdev.core}
-  :uberjar-name "webdev.jar" 
-  
+  :uberjar-name "webdev.jar"
+
   :main webdev.core
   :profiles {:dev
              {:main webdev.core/-dev-main}})
